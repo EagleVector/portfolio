73179ee25261b330c2b4c614496c9c73a69871a1
EagleVector <kumarsubhambili3@gmail.com>
2023-09-18 09:48:57 +0530

    framer motion pkg deps resolve and lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,36 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from '@/components/header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Subham Kumar',
-  description: 'Software Developer with 1 year of experience',
-  icons: {
-    icon:['/favicon.ico?v=4'],
-    apple:['/apple-touch-icon.png?v=4'],
-    shortcut:['/apple-touch-icon.png']
-  }
-}
+	title: 'Subham Kumar',
+	description: 'Software Developer with 1 year of experience',
+	icons: {
+		icon: ['/favicon.ico?v=4'],
+		apple: ['/apple-touch-icon.png?v=4'],
+		shortcut: ['/apple-touch-icon.png']
+	}
+};
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative`}
-      >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+	return (
+		<html lang="en">
+			<body
+				className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}
+			>
+				<div className="bg-[#f4d1d2] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 
-        {children}
-      </body>
-    </html>
-  )
+				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
+				<Header />
+				{children}
+			</body>
+		</html>
+	);
 }
