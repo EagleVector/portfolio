d0e53cb563dd5d767e77cb7f3da41997295c52ff
EagleVector <kumarsubhambili3@gmail.com>
2023-09-21 19:24:35 +0530

    Client side input val with HTML and resolve deps

--- components/contact.tsx
+++ components/contact.tsx
@@ -5,9 +5,11 @@ import React from 'react'
 import SectionHeading from './section-heading'
 import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion';
+import { sendEmail } from '@/actions/sendEmail';
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+
   return (
     <motion.section 
       ref={ref}
@@ -35,15 +37,26 @@ export default function Contact() {
         or fill this form.
       </p>
 
-      <form className='mt-10 flex flex-col'>
+      <form 
+        className='mt-10 flex flex-col' 
+        action={async (formData) => {
+          await sendEmail(formData);
+        }}
+      >
         <input 
-          className='h-14 rounded-lg borderBlack px-4' 
+          className='h-14 rounded-lg borderBlack px-4'
+          name='senderEmail' 
           type="email" 
+          required
+          maxLength={500}
           placeholder='Your Email'
         />
         <textarea 
           className='h-52  my-3 rounded-lg borderBlack p-4' 
+          name='message'
           placeholder='Your message'
+          required
+          maxLength={5000}
         />
         <button type="submit" className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-102'>
           Submit 
