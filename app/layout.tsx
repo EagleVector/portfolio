227ac9723ed4e10955e0ca29f947c39176193fc1
EagleVector <kumarsubhambili3@gmail.com>
2023-09-22 09:58:03 +0530

    Footer component render

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
 				<ActiveSectionContextProvider>
 					<Header />
 					{children}
+					<Footer />
 					<Toaster position='top-right' />
 				</ActiveSectionContextProvider>
 			</body>
