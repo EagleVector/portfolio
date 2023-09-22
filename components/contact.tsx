2409bffe60ce23ed8047dfb06702faef2b38ce95
EagleVector <kumarsubhambili3@gmail.com>
2023-09-22 09:40:18 +0530

    React Hot Toast loading on email send

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,9 +3,10 @@
 import { useSectionInView } from '@/lib/hooks';
 import React from 'react'
 import SectionHeading from './section-heading'
-import { FaPaperPlane } from 'react-icons/fa'
 import { motion } from 'framer-motion';
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -40,7 +41,13 @@ export default function Contact() {
       <form 
         className='mt-10 flex flex-col' 
         action={async (formData) => {
-          await sendEmail(formData);
+          const { data, error } = await sendEmail(formData);
+
+        if (error) {
+          toast.error(error);
+          return;
+        }
+        toast.success("Email sent successfully")
         }}
       >
         <input 
@@ -58,13 +65,7 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <button type="submit" className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-105 hover:scale-105 hover:bg-gray-950 active:scale-102'>
-          Submit 
-          <FaPaperPlane className='text-xs 
-          opacity-70 transition-all
-          group-hover:translate-x-1
-          group-hover:-translate-y-1  '/>{" "}
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   )
