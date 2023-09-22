779e06c7c0d90c53e0a0dd038afc6d742869d6d4
EagleVector <kumarsubhambili3@gmail.com>
2023-09-22 09:57:37 +0530

    Footer Component

---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+      <small className='mb-2 block text-xs'>&copy; 2023 kumarsubhambili3@gmail.com. All rights reserved.</small>
+      <p className='text-xs'>
+        <span className='font-semibold'>About this website:</span> Built with React and Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend. Hosted on Vercel.
+      </p>
+    </footer>
+  )
+}
